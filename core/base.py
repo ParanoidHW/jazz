fix errors in broadcast

core/base.py
@@ -10,7 +10,7 @@ import numpy as np
 
 
 class BaseZhangliang(object):
-    def __init__(self, data, dtype=np.float32, requires_grad=False):
+    def __init__(self, data, dtype=np.float64, requires_grad=False):
         self._zhi = np.array(data, dtype=dtype)
         self.requires_grad = requires_grad
         self._tidu = np.zeros_like(self._zhi)

core/tensor.py
@@ -14,7 +14,7 @@ from utils.misc import additive_broadcast_analysis, multiplicative_broadcast_ana
 
 
 class Zhangliang(BaseZhangliang):
-    def __init__(self, data, dtype=np.float32, requires_grad=False):
+    def __init__(self, data, dtype=np.float64, requires_grad=False):
         if isinstance(data, Zhangliang):
             data = data.values
         elif np.isscalar(data):
@@ -22,23 +22,23 @@ class Zhangliang(BaseZhangliang):
         super(Zhangliang, self).__init__(data, dtype, requires_grad)
 
     @classmethod
-    def zeros(cls, shape, dtype=np.float32, requires_grad=False):
+    def zeros(cls, shape, dtype=np.float64, requires_grad=False):
         zeros_ = np.zeros(shape, dtype=dtype)
         return cls(zeros_, requires_grad=requires_grad)
 
     @classmethod
-    def ones(cls, shape, dtype=np.float32, requires_grad=False):
+    def ones(cls, shape, dtype=np.float64, requires_grad=False):
         ones_ = np.ones(shape, dtype=dtype)
         return cls(ones_, requires_grad=requires_grad)
 
     @classmethod
-    def zeros_like(cls, data, dtype=np.float32, requires_grad=False):
+    def zeros_like(cls, data, dtype=np.float64, requires_grad=False):
         shape = data.shape
         zeros_ = np.zeros(shape, dtype=dtype)
         return cls(zeros_, requires_grad=requires_grad)
 
     @classmethod
-    def ones_like(cls, data, dtype=np.float32, requires_grad=False):
+    def ones_like(cls, data, dtype=np.float64, requires_grad=False):
         shape = data.shapes
         ones_ = np.ones(shape, dtype=dtype)
         return cls(ones_, requires_grad=requires_grad)
@@ -50,10 +50,10 @@ class Zhangliang(BaseZhangliang):
         elif np.isscalar(data):
             return cls([data], dtype=np.int32, requires_grad=requires_grad)
         elif isinstance(data, (list, tuple)):
-            return cls(data, dtype=np.float32, requires_grad=requires_grad)
+            return cls(data, dtype=np.float64, requires_grad=requires_grad)
         elif isinstance(data, collections.Iterable):
             data = np.array(data)
-            return cls(data, dtype=np.float32, requires_grad=requires_grad)
+            return cls(data, dtype=np.float64, requires_grad=requires_grad)
         else:
             raise TypeError
 
@@ -259,7 +259,7 @@ def zl_mul_grad(output, x, y):
         grads = output.grad * y.values
         if len(axes_to_reduce[0]) > 0:
             grads = np.sum(grads, axis=axes_to_reduce[0])
-        y.assign_grad(np.reshape(grads, x.shape))
+        x.assign_grad(np.reshape(grads, x.shape))
     if isinstance(y, Zhangliang) and y.requires_grad:
         grads = output.grad * x.values
         if len(axes_to_reduce[1]) > 0:

unitest/test_misc.py
@@ -2,10 +2,11 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
+import numpy as np
+
 
 def test_broadcast_rule():
     from utils.misc import additive_broadcast_analysis, multiplicative_broadcast_analysis
-    import numpy as np
 
     def eltwise(ashape, bshape):
         a = np.ones(ashape)
@@ -69,7 +70,15 @@ def test_broadcast_rule():
     b_shape = (2, )
     check_elt_shape(a_shape, b_shape)
 
+    b_shape = (5,)
+    check_elt_shape(a_shape, b_shape)
+
+    a_shape = (1,)
+    b_shape = (2,)
+    check_elt_shape(a_shape, b_shape)
+
     # matmul
+    a_shape = (2, 3, 4, 5)
     b_shape = (1,)
     check_prod_shape(a_shape, b_shape)
 
@@ -100,4 +109,8 @@ def test_broadcast_rule():
 
     a_shape = (5, )
     b_shape = (2, 3, 1, 5)
+    check_prod_shape(a_shape, b_shape)
+
+    a_shape = (1,)
+    b_shape = (2,)
     check_prod_shape(a_shape, b_shape)

utils/misc.py
@@ -6,6 +6,50 @@ import numbers
 import collections
 
 
+def expand_dim_as(old_shape, ref_shape):
+    ndim_a = len(old_shape)
+    ndim_ref = len(ref_shape)
+    new_shape = list([1] * (ndim_ref - ndim_a)) + list(old_shape)
+    return new_shape
+
+
+def check_additive_dim_compatible(a_shape, ref_shape):
+    ndim_a = len(a_shape)
+    ndim_ref = len(ref_shape)
+    assert ndim_a == ndim_ref, 'Dimension not equal: {} vs {}.'.format(ndim_a, ndim_ref)
+    compatible = True
+    aggregated_axes = []
+    for i, (dim_a, dim_ref) in enumerate(zip(a_shape, ref_shape)):
+        # `ref_shape` is supposed to be the shape of the operator result.
+        # So ref_shape[i] >= a_shape[i] at every where
+        if dim_a != dim_ref and dim_a != 1:
+            compatible = False
+            return compatible
+        elif dim_a == 1:
+            aggregated_axes.append(i)
+    return compatible, aggregated_axes
+
+
+def check_multiplicative_dim_compatible(a_shape, ref_shape):
+    ndim_a = len(a_shape)
+    ndim_ref = len(ref_shape)
+    assert ndim_a == ndim_ref, 'Dimension not equal: {} vs {}.'.format(ndim_a, ndim_ref)
+    # The length of both input shapes must greater than or equal with 2.
+    # Otherwise the operator cannot perform.
+    # One of the last dim in `a_shape` should be the same with the corresponding dim of `ref_shape`.
+    compatible = (a_shape[-1] == ref_shape[-1]) or (a_shape[-2] == ref_shape[-2])
+    aggregated_axes = []
+    for i, (dim_a, dim_ref) in enumerate(zip(a_shape[:-2], ref_shape[:-2])):
+        # `ref_shape` is supposed to be the shape of the operator result.
+        # So ref_shape[i] >= a_shape[i] at every where
+        if dim_a != dim_ref and dim_a != 1:
+            compatible = False
+            return compatible
+        elif dim_a == 1:
+            aggregated_axes.append(i)
+    return compatible, aggregated_axes
+
+
 def additive_broadcast_analysis(input_shapes, output_shape):
     """
     Analyze and determine the broadcast method. Return a tuple of the axes to be reduced
@@ -14,45 +58,13 @@ def additive_broadcast_analysis(input_shapes, output_shape):
     :param output_shape: the shape of the output
     :return:
     """
-
-    def data_shape_comp(a_shape, b_shape):
-        """
-        Compare a single shape `a_shape` and another shape `b_shape`， determine whether can be broadcast.
-        :param a_shape:
-        :param b_shape:
-        :return:
-        """
-        # First check whether all axes reduced.
-        axes_to_reduce = []
-        incompatible = False
-        if len(a_shape) == 1:
-            if a_shape[0] == 1:
-                axes_to_reduce = tuple(range(len(b_shape)))
-                incompatible = False
-            elif len(b_shape) > 1:
-                axes_to_reduce = []
-                incompatible = True
-        # Check whether the two shapes have the same length.
-        # If not, the broadcast is not possible.
-        elif len(a_shape) != len(b_shape):
-            axes_to_reduce = []
-            incompatible = True
-        # Check for each axis in both shapes. If broadcast is satisfied, the different
-        # axes in `a_shape` should have the dimension 1.
-        else:
-            axes_to_reduce = []
-            incompatible = False
-            for i, (a_axis, b_axis) in enumerate(zip(a_shape, b_shape)):
-                if a_axis != b_axis:
-                    incompatible = incompatible or (a_axis != 1)
-                    axes_to_reduce.append(i)
-        return tuple(axes_to_reduce), incompatible
-
     reduced_axes = []
     for input_shape in input_shapes:
-        red_ax, invalid = data_shape_comp(input_shape, output_shape)
-        if not invalid:
-            reduced_axes.append(red_ax)
+        # red_ax, invalid = data_shape_comp(input_shape, output_shape)
+        new_shape = expand_dim_as(input_shape, output_shape)
+        valid, axes_aggregated = check_additive_dim_compatible(new_shape, output_shape)
+        if valid:
+            reduced_axes.append(tuple(axes_aggregated))
         else:
             raise ArithmeticError
 
@@ -67,43 +79,20 @@ def multiplicative_broadcast_analysis(input_shapes, output_shape):
         :param output_shape: the shape of the output
         :return:
         """
-
-    def data_shape_comp(a_shape, b_shape):
-        """
-        Compare a single shape `a_shape` and another shape `b_shape` can be broadcast.
-        :param a_shape:
-        :param b_shape:
-        :return:
-        """
-        a_dim = len(a_shape)
-        b_dim = len(b_shape)
-        # In case of (m, n) X (n, ) = (m, )
-        if a_dim > b_dim:
-            b_shape = b_shape + (1, )
-
-        # First check whether all axes reduced.
-        if a_dim == 1:
-            axes_to_reduce = tuple(range(b_dim))
-            incompatible = False
-        else:
-            axes_to_reduce = []
-            incompatible = False
-
-            if b_dim > a_dim:
-                axes_to_reduce = list(range(b_dim-a_dim))
-            for i in range(a_dim-2):
-                neg_idx = i - a_dim
-                if a_shape[neg_idx] != b_shape[neg_idx]:
-                    incompatible = a_shape[neg_idx] != 1
-                    if a_shape[neg_idx] == 1:
-                        axes_to_reduce.append(i)
-        return tuple(axes_to_reduce), incompatible
+    # (m_1, m_2, ..., m_k, n) X (n, ) = (m_1, m_2, ..., m_k) happens.
+    input_shapes_ = [list(a) for a in input_shapes]
+    output_shape_ = list(output_shape)
+    if len(input_shapes_[1]) == 1:
+        input_shapes_[1] += [1]
+        output_shape_ += [1]
 
     reduced_axes = []
-    for input_shape in input_shapes:
-        red_ax, invalid = data_shape_comp(input_shape, output_shape)
-        if not invalid:
-            reduced_axes.append(red_ax)
+    for input_shape in input_shapes_:
+        # red_ax, invalid = data_shape_comp(input_shape, output_shape)
+        new_shape = expand_dim_as(input_shape, output_shape_)
+        valid, axes_aggregated = check_multiplicative_dim_compatible(new_shape, output_shape_)
+        if valid:
+            reduced_axes.append(tuple(axes_aggregated))
         else:
             raise ArithmeticError
 
@@ -127,104 +116,3 @@ def recover_dim(ori_shape, tar_shape, dim=None, keepdims=False):
     for d in dim:
         new_shape[d] = 1
     return new_shape
-
-
-if __name__ == '__main__':
-    import numpy as np
-
-    def eltwise(ashape, bshape):
-        a = np.ones(ashape)
-        b = np.ones(bshape)
-        c = a + b
-        return c.shape
-
-    def mult(ashape, bshape):
-        a = np.ones(ashape)
-        b = np.ones(bshape)
-        c = np.matmul(a, b)
-        return c.shape
-
-    def check_elt_shape(ashape, bshape):
-        try:
-            print('-----------------------------------------------------------------------------------')
-            elt_shape = eltwise(ashape, bshape)
-            print('NP allowed elt operator for {} and {}.'.format(ashape, bshape))
-            axes_to_reduce = additive_broadcast_analysis((a_shape, b_shape), elt_shape)
-            print('{} + {} = {}'.format(ashape, bshape, elt_shape))
-            output_info_pattern = '\tShape A: {}, axes to reduce: {}'.format(ashape, axes_to_reduce[0])
-            print(output_info_pattern)
-            output_info_pattern = '\tShape B: {}, axes to reduce: {}'.format(bshape, axes_to_reduce[1])
-            print(output_info_pattern)
-        except ValueError:
-            print('NP elt operator for {} and {} failed.'.format(ashape, bshape))
-        except ArithmeticError:
-            print('Something wrong with analysis elt operator for {} and {}.'.format(ashape, bshape))
-
-
-    def check_prod_shape(ashape, bshape):
-        try:
-            print('-----------------------------------------------------------------------------------')
-            prod_shape = mult(ashape, bshape)
-            print('NP allowed prod operator for {} and {}.'.format(ashape, bshape))
-            axes_to_reduce = multiplicative_broadcast_analysis((a_shape, b_shape), prod_shape)
-            print('{} X {} = {}'.format(ashape, bshape, prod_shape))
-            output_info_pattern = '\tShape A: {}, axes to reduce: {}'.format(ashape, axes_to_reduce[0])
-            print(output_info_pattern)
-            output_info_pattern = '\tShape B: {}, axes to reduce: {}'.format(bshape, axes_to_reduce[1])
-            print(output_info_pattern)
-        except ValueError:
-            print('NP prod operator for {} and {} failed.'.format(ashape, bshape))
-        except ArithmeticError:
-            print('Something wrong with analysis prod operator for {} and {}.'.format(ashape, bshape))
-
-
-    a_shape = (2, 3, 4, 5)
-
-    # element-wise
-    b_shape = (1, )
-    check_elt_shape(a_shape, b_shape)
-
-    b_shape = (1, 1, 1, 5)
-    check_elt_shape(a_shape, b_shape)
-
-    b_shape = (2, 1, 4, 1)
-    check_elt_shape(a_shape, b_shape)
-
-    b_shape = (2, 3, 4, 5)
-    check_elt_shape(a_shape, b_shape)
-
-    b_shape = (2, )
-    check_elt_shape(a_shape, b_shape)
-
-    # matmul
-    b_shape = (1,)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (5,)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (3, 5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (1, 1, 5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (2, 1, 5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (1, 3, 5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    b_shape = (2, 3, 4, 2, 3, 5, 6)
-    check_prod_shape(a_shape, b_shape)
-
-    a_shape = (2, 3, 4, 2, 3, 5, 6)
-    b_shape = (2, 1, 4, 5)
-    check_prod_shape(a_shape, b_shape)
-
-    a_shape = (5, )
-    b_shape = (2, 3, 1, 5)
-    check_prod_shape(a_shape, b_shape)