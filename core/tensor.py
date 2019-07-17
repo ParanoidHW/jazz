add more gradient function test, fix some bugs

core/tensor.py
@@ -194,10 +194,10 @@ def zl_add_grad(output, x, y):
     output_shape = output.shape
     axes_to_reduce = additive_broadcast_analysis(inputs_shapes, output_shape)
     if isinstance(x, Zhangliang) and x.requires_grad:
-        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[0], x.shape)
+        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[0], x_.shape)
         x.assign_grad(grads)
     if isinstance(y, Zhangliang) and y.requires_grad:
-        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[1], y.shape)
+        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[1], y_.shape)
         y.assign_grad(grads)
 
 
@@ -218,10 +218,10 @@ def zl_sub_grad(output, x, y):
     output_shape = output.shape
     axes_to_reduce = additive_broadcast_analysis(inputs_shapes, output_shape)
     if isinstance(x, Zhangliang) and x.requires_grad:
-        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[0], x.shape)
+        grads = aggregate_and_reshape_grad(output.grad, axes_to_reduce[0], x_.shape)
         x.assign_grad(grads)
     if isinstance(y, Zhangliang) and y.requires_grad:
-        grads = aggregate_and_reshape_grad(-output.grad, axes_to_reduce[1], y.shape)
+        grads = aggregate_and_reshape_grad(-output.grad, axes_to_reduce[1], y_.shape)
         y.assign_grad(grads)
 
 
@@ -244,11 +244,11 @@ def zl_mul_grad(output, x, y):
     if isinstance(x, Zhangliang) and x.requires_grad:
         # The output definitely can broadcast with each input.
         grads = output.grad * y.values
-        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[0], x.shape)
+        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[0], x_.shape)
         x.assign_grad(grads)
     if isinstance(y, Zhangliang) and y.requires_grad:
         grads = output.grad * x.values
-        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[1], y.shape)
+        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[1], y_.shape)
         y.assign_grad(grads)
 
 
@@ -271,11 +271,11 @@ def zl_truediv_grad(output, x, y):
     if isinstance(x, Zhangliang) and x.requires_grad:
         # The output definitely can broadcast with each input.
         grads = output.grad / y.values
-        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[0], x.shape)
+        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[0], x_.shape)
         x.assign_grad(grads)
     if isinstance(y, Zhangliang) and y.requires_grad:
         grads = - output.grad * x.values / (y.values ** 2)
-        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[1], y.shape)
+        grads = aggregate_and_reshape_grad(grads, axes_to_reduce[1], y_.shape)
         y.assign_grad(np.reshape(grads, y.shape))
 
 
@@ -313,6 +313,8 @@ def zl_matmul_grad(output, x, y):
     y.assign_grad(np.sum(b_grad, axis=axes_to_reduce[1]))
 
 
+# TODO: `Zhangliang` does not seem to support for `complex` data type.
+# So the inputs of the `pow` should be positive.
 @ctx_register(op_name='pow')
 def zl_pow(x, y):
     local_requires_grad = is_zhangliang_requires_grad(x) or is_zhangliang_requires_grad(y)
@@ -608,7 +610,8 @@ def zl_abs(x):
 
 @grad_register(op_name='abs')
 def zl_abs_grad(output, x):
-    values = np.where(x.values, x.values, 0.)
+    values = np.where(x.values > 0, 1., -1.)
+    values = np.where(x.values, values, 0.)
     if isinstance(x, Zhangliang) and x.requires_grad:
         x.assign_grad(output.grad * values)
 
@@ -674,10 +677,18 @@ def zl_log1p_grad(output, x):
 
 
 def grad_minmax(xin, zout, grad, dim=None, keepdims=False):
-    new_shape = recover_dim(xin.shape, zout.shape, dim=dim, keepdims=keepdims)
-    zout = np.reshape(zout, newshape=new_shape)
+    inputs_shapes = xin.shape
+    reduced_shapes = list(inputs_shapes)
+    if dim is None:
+        dim = list(range(xin.ndim))
+
+    for i in dim:
+        # We do not need the grad has the same shape as the values, but the broadcast shape is necessary.
+        reduced_shapes[i] = 1
+
+    zout = np.reshape(zout, newshape=reduced_shapes)
     max_value_map = xin == zout
-    nmax = np.sum(max_value_map, axis=dim)
+    nmax = np.sum(max_value_map, axis=tuple(dim))
     values = grad * max_value_map / nmax
     return values
 
@@ -749,7 +760,7 @@ def zl_clamp(x, xmin=0., xmax=1.):
 @grad_register(op_name='clamp')
 def zl_clamp_grad(output, x, xmin=0., xmax=1.):
     if isinstance(x, Zhangliang) and x.requires_grad:
-        valid_region = np.logical_and(x.values != xmin, x.values != xmax)
+        valid_region = np.logical_and(x.values >= xmin, x.values <= xmax)
         values = output.grad * valid_region
         x.assign_grad(values)
 
@@ -822,6 +833,8 @@ def zl_arcsin(x):
 
 @grad_register(op_name='arcsin')
 def zl_arcsin_grad(output, x):
+    # TODO: the error becomes significant when the `x.values` are close to -1 and 1.
+    # How to make it stable?
     if isinstance(x, Zhangliang) and x.requires_grad:
         values = output.grad / (np.sqrt(1 - x.values ** 2))
         x.assign_grad(values)
@@ -837,6 +850,8 @@ def zl_arccos(x):
 
 @grad_register(op_name='arccos')
 def zl_arccos_grad(output, x):
+    # TODO: the error becomes significant when the `x.values` are close to -1 and 1.
+    # How to make it stable?
     if isinstance(x, Zhangliang) and x.requires_grad:
         values = - output.grad / (np.sqrt(1 - x.values ** 2))
         x.assign_grad(values)
@@ -868,7 +883,7 @@ def zl_sinh(x):
 @grad_register(op_name='sinh')
 def zl_sinh_grad(output, x):
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad * np.cosh(a_.values)
+        values = output.grad * np.cosh(x.values)
         x.assign_grad(values)
 
 
@@ -883,7 +898,7 @@ def zl_cosh(x):
 @grad_register(op_name='cosh')
 def zl_cosh_grad(output, x):
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad * np.sinh(a_.values)
+        values = output.grad * np.sinh(x.values)
         x.assign_grad(values)
 
 
@@ -898,7 +913,7 @@ def zl_tanh(x):
 @grad_register(op_name='tanh')
 def zl_tanh_grad(output, x):
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad / (np.cosh(a_.values) ** 2)
+        values = output.grad / (np.cosh(x.values) ** 2)
         x.assign_grad(values)
 
 
@@ -913,7 +928,7 @@ def zl_arcsinh(x):
 @grad_register(op_name='arcsinh')
 def zl_arcsinh_grad(output, x):
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad / np.sqrt(a_.values ** 2 + 1)
+        values = output.grad / np.sqrt(x.values ** 2 + 1)
         x.assign_grad(values)
 
 
@@ -927,8 +942,10 @@ def zl_arccosh(x):
 
 @grad_register(op_name='arccosh')
 def zl_arccosh_grad(output, x):
+    # TODO: the error becomes significant when the `x.values` are close to 1.
+    # How to make it stable?
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad / np.sqrt(a_.values ** 2 - 1)
+        values = output.grad / np.sqrt(x.values ** 2 - 1)
         x.assign_grad(values)
 
 
@@ -942,8 +959,10 @@ def zl_arctanh(x):
 
 @grad_register(op_name='arctanh')
 def zl_arctanh_grad(output, x):
+    # TODO: the error becomes significant when the `x.values` are close to -1 and 1.
+    # How to make it stable?
     if isinstance(x, Zhangliang) and x.requires_grad:
-        values = output.grad / (1. - a_.values ** 2)
+        values = output.grad / (1. - x.values ** 2)
         x.assign_grad(values)
 
 
@@ -978,6 +997,9 @@ def zl_unsqueeze_grad(output, x, dim):
     if isinstance(input, Zhangliang) and x.requires_grad:
         values = np.reshape(output.grad, x.shape)
         x.assign_grad(values)
+
+
+# ---------------- array-like functions -----------------
 
 
 @ctx_register(op_name='concat')

unitest/test_tensor_ops.py
@@ -14,86 +14,16 @@ RTOL = 1e-6
 EPS = 1e-6
 
 
-def arg_space():
-    scalar = 2.0
-    vector = np.random.randn(4)
-    mat = np.random.randn(3, 4)
-    mat2 = np.random.randn(1, 4)
-    allargs = [scalar, vector, mat, mat2]
-    for arg1, arg2 in product(allargs, allargs):
-        yield arg1, arg2
-
-
 def array_close(a, b):
     array_flag = np.logical_or(np.abs(a - b) < TOL, np.abs(a - b) / np.abs(a + b) < RTOL)
-    return np.all(array_flag)
-
-
-class VarianceSpace(object):
-    def __init__(self, data):
-        self.data = data
-        self.shape = data.shape
-
-    def __len__(self):
-        return np.size(self.data)
-
-    def __getitem__(self, index):
-        variance = np.random.randn(self.data.shape)
-        return variance
+    return np.all(array_flag) and \
+            (a.shape == b.shape) and \
+           not np.any(np.isnan(a)) and \
+            not np.any(np.isnan(b))
 
 
 def approx_numeric_grad(z_p, z_m):
     return (z_p - z_m) / EPS
-
-
-def check_element_binary_numeric_grad(op_name, a, b, *args, **kwargs):
-    fn = func_lib[op_name]
-    gn = grad_lib[op_name]
-    x = Zhangliang(a, requires_grad=True)
-    y = Zhangliang(b, requires_grad=True)
-    z = fn(x, y, *args, **kwargs)
-    z.assign_grad(np.ones_like(z))
-    gn(z, x, y, *args, **kwargs)
-    axes_to_reduce = additive_broadcast_analysis([x.shape, y.shape], z.shape)
-
-    for i in range(10):
-        x_var = np.random.rand(*(x.shape)) * EPS/2
-        x_var = np.where(x_var, x_var, EPS/2)
-
-        x_plus = Zhangliang(x + x_var)
-        x_minus = Zhangliang(x - x_var)
-
-        z_plus = fn(x_plus, y, *args, **kwargs)
-        z_minus = fn(x_minus, y, *args, **kwargs)
-
-        x_numer_grad = (z_plus.values - z_minus.values) / (2 * x_var)
-        x_numer_grad = np.sum(x_numer_grad, axis=axes_to_reduce[0])
-
-        if not array_close(x_numer_grad, x.grad):
-            print('Test derivative check of `{}` w.r.t. `x` ({}/10) failed.\n' \
-                  ' Analytic: {}\n Numeric: {}'.format(op_name, i, x, y, x.grad, x_numer_grad))
-        else:
-            print('Test derivative check of `{}` w.r.t. `x` ({}/10) passed.'.format(op_name, i))
-
-    gn(z, x, y, *args, **kwargs)
-    for i in range(10):
-        y_var = np.random.rand(*(y.shape)) * EPS / 2
-        y_var = np.where(y_var, y_var, EPS / 2)
-
-        y_plus = Zhangliang(y + y_var, dtype=np.float64)
-        y_minus = Zhangliang(y - y_var, dtype=np.float64)
-
-        z_plus = fn(x, y_plus, *args, **kwargs)
-        z_minus = fn(x, y_minus, *args, **kwargs)
-
-        y_numer_grad = (z_plus.values - z_minus.values) / (2 * y_var)
-        y_numer_grad = np.sum(y_numer_grad, axis=axes_to_reduce[1])
-
-        if not array_close(y_numer_grad, y.grad):
-            print('Test derivative check of `{}` w.r.t. `y` ({}/10) failed.\n' \
-                  ' Analytic: {}\n Numeric: {}'.format(op_name, i, x, y, y.grad, y_numer_grad))
-        else:
-            print('Test derivative check of `{}` w.r.t. `y` ({}/10) passed.'.format(op_name, i))
 
 
 def test_check_forward_func_and_backkward_func():
@@ -104,12 +34,153 @@ def test_check_forward_func_and_backkward_func():
     assert len(diff) == 0
 
 
-def test_check_binary_func():
-    get_arg = arg_space()
-    op_list = {'add', 'sub', 'mul', 'div', 'minimum', 'maximum'}
-    for arg1, arg2 in get_arg:
-        for op_name in sorted(op_list):
+def test_element_binary_func():
+    def binary_arg_space():
+        scalar = 2.0
+        vector = np.random.randn(4)
+        mat = np.random.randn(3, 4)
+        mat2 = np.random.randn(1, 4)
+        allargs = [scalar, vector, mat, mat2]
+        for arg1, arg2 in product(allargs, allargs):
+            yield arg1, arg2
+
+    def check_grad(op_name, a, b, *args, **kwargs):
+        max_trial = 5
+        fn = func_lib[op_name]
+        gn = grad_lib[op_name]
+        x = Zhangliang(a, requires_grad=True)
+        y = Zhangliang(b, requires_grad=True)
+        z = fn(x, y, *args, **kwargs)
+        z.assign_grad(np.ones_like(z))
+        gn(z, x, y, *args, **kwargs)
+        axes_to_reduce = additive_broadcast_analysis([x.shape, y.shape], z.shape)
+
+        for i in range(max_trial):
+            x_var = np.random.rand(*(x.shape)) * EPS / 2
+            x_var = np.where(x_var, x_var, EPS / 2)
+
+            x_plus = Zhangliang(x + x_var)
+            x_minus = Zhangliang(x - x_var)
+
+            z_plus = fn(x_plus, y, *args, **kwargs)
+            z_minus = fn(x_minus, y, *args, **kwargs)
+
+            x_numer_grad = (z_plus.values - z_minus.values) / (2 * x_var)
+            x_numer_grad = np.sum(x_numer_grad, axis=axes_to_reduce[0])
+            if np.isscalar(x_numer_grad):
+                x_numer_grad = np.array([x_numer_grad], dtype=np.float64)
+            x_numer_grad = np.reshape(x_numer_grad, x.shape)
+
+            if not array_close(x_numer_grad, x.grad):
+                print('Test derivative of `{}` w.r.t. `x` ({}/{}) failed.\n' \
+                      ' Analytic: {}\n Numeric: {}'.format(op_name, i, max_trial, x.grad, x_numer_grad))
+            else:
+                print('Test derivative of `{}` w.r.t. `x` ({}/{}) passed.'.format(op_name, i, max_trial))
+
+        gn(z, x, y, *args, **kwargs)
+        for i in range(max_trial):
+            y_var = np.random.rand(*(y.shape)) * EPS / 2
+            y_var = np.where(y_var, y_var, EPS / 2)
+
+            y_plus = Zhangliang(y + y_var, dtype=np.float64)
+            y_minus = Zhangliang(y - y_var, dtype=np.float64)
+
+            z_plus = fn(x, y_plus, *args, **kwargs)
+            z_minus = fn(x, y_minus, *args, **kwargs)
+
+            y_numer_grad = (z_plus.values - z_minus.values) / (2 * y_var)
+            y_numer_grad = np.sum(y_numer_grad, axis=axes_to_reduce[1])
+
+            if np.isscalar(y_numer_grad):
+                y_numer_grad = np.array([y_numer_grad], dtype=np.float64)
+            y_numer_grad = np.reshape(y_numer_grad, y.shape)
+
+            if not array_close(y_numer_grad, y.grad):
+                print('Test derivative of `{}` w.r.t. `y` ({}/{}) failed.\n' \
+                      ' Analytic: {}\n Numeric: {}'.format(op_name, i, max_trial, y.grad, y_numer_grad))
+            else:
+                print('Test derivative of `{}` w.r.t. `y` ({}/{}) passed.'.format(op_name, i, max_trial))
+
+    op_list = ['add', 'sub', 'mul', 'div', 'minimum', 'maximum', 'pow']
+
+    for op_name in sorted(op_list):
+        get_arg = binary_arg_space()
+        for arg1, arg2 in get_arg:
+            if op_name == 'pow':
+                arg1, arg2 = np.abs(arg1), np.abs(arg2)
+                # 'Zhangliang' does not support `complex` data type right now.
+                # So make sure the input of the `pow` is positive.
+
             print('---------------------------------------------------------')
             print('Arg1: {}\nArg2: {}\nop: {}'.format(arg1, arg2, op_name))
-            check_element_binary_numeric_grad(op_name, arg1, arg2)
+            check_grad(op_name, arg1, arg2)
+
+
+def test_arg_free_unary_func():
+    def unary_arg_space():
+        scalar = 2.0
+        vector = np.random.randn(4)
+        mat = np.random.randn(3, 4)
+        mat2 = np.random.randn(1, 4, 5)
+        allargs = [scalar, vector, mat, mat2]
+        for arg1 in allargs:
+            yield arg1
+
+    def check_grad(op_name, a, *args, **kwargs):
+        max_trial = 5
+        fn = func_lib[op_name]
+        gn = grad_lib[op_name]
+        x = Zhangliang(a, requires_grad=True)
+        z = fn(x, *args, **kwargs)
+        z.assign_grad(np.ones_like(z))
+        gn(z, x, *args, **kwargs)
+        axes_to_reduce = additive_broadcast_analysis([x.shape], z.shape)
+
+        for i in range(max_trial):
+            x_var = np.random.rand(*(x.shape)) * EPS / 2
+            x_var = np.where(x_var, x_var, EPS / 2)
+
+            x_plus = Zhangliang(x + x_var)
+            x_minus = Zhangliang(x - x_var)
+
+            z_plus = fn(x_plus, *args, **kwargs)
+            z_minus = fn(x_minus, *args, **kwargs)
+
+            x_numer_grad = (z_plus.values - z_minus.values) / (2 * x_var)
+            x_numer_grad = np.sum(x_numer_grad, axis=axes_to_reduce[0])
+            if np.isscalar(x_numer_grad):
+                x_numer_grad = np.array([x_numer_grad], dtype=np.float64)
+            x_numer_grad = np.reshape(x_numer_grad, x.shape)
+
+            if not array_close(x_numer_grad, x.grad):
+                print('Test derivative of `{}` w.r.t. `x` ({}/{}) failed.\n' \
+                      ' Analytic: {}\n Numeric: {}'.format(op_name, i, max_trial, x.grad, x_numer_grad))
+            else:
+                print('Test derivative of `{}` w.r.t. `x` ({}/{}) passed.'.format(op_name, i, max_trial))
+
+    op_list = ['neg', 'abs', 'clamp', 'sin', 'cos', 'tan',
+               'sinh', 'cosh', 'tanh', 'arctan', 'arcsinh',
+               'arcsin', 'arccos',
+               'arccosh', 'arctanh',
+               'log', 'log2', 'log10', 'log1p']
+
+    for op_name in sorted(op_list):
+        get_arg = unary_arg_space()
+        for arg1 in get_arg:
+            if op_name in ('arcsin', 'arccos', 'arctanh'):
+                # TODO: Unstable near boundary
+                arg1 = np.clip(arg1, a_min=-0.99999, a_max=.99999)
+            elif op_name == 'arccosh':
+                # TODO: Unstable near boundary
+                arg1 = np.maximum(arg1, 1.00001)
+            elif op_name in ['log', 'log2', 'log10', 'log1p']:
+                # TODO: Unstable near boundary
+                arg1 = np.abs(arg1)
+            print('---------------------------------------------------------')
+            print('Arg1: {}\nop: {}'.format(arg1, op_name))
+            check_grad(op_name, arg1)
+
+
+def test_check_shape_changed_unary_func():
+    op_list = {'max', 'min',  'reduce_mean', 'reduce_sum', 'squeeze', 'unsqueeze'}
 